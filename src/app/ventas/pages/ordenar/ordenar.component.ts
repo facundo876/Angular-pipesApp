d24342ad78b6ebc9cbe6296fd30ebc1990c1ebec
Facundo Arce <40859125+facundo876@users.noreply.github.com>
2022-04-27 02:26:43 -0300

    pipes personalizados , talbes primeng  y sort

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,16 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
+  orden:string = '';
   isMayus:boolean = false;
+
+  heroes:Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    }
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   enMayusculas(){
     this.isMayus = !this.isMayus;
+  }
+
+  cambiarOrden(orden:string){
+
+    this.orden = orden;
   }
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BasicosComponent } from './pages/basicos/basicos.component';
-import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
-import { NumerosComponent } from './pages/numeros/numeros.component';
-import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+import { NgModule } from '@angular/core';
+
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
+import { BasicosComponent } from './pages/basicos/basicos.component';
+import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
+import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+import { NumerosComponent } from './pages/numeros/numeros.component';
+
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
@@ -16,7 +20,9 @@ import { MayusculasPipe } from './pipes/mayusculas.pipe';
     NoComunesComponent,
     NumerosComponent,
     OrdenarComponent,
-    MayusculasPipe
+    MayusculasPipe,
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports:[
     BasicosComponent,
